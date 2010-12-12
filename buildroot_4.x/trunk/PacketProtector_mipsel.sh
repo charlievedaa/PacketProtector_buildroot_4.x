2f93a74f4be55b6b5506308fa7f78b5d8600b1f0
ciscostu <ciscostu@61e7896b-eb2b-0410-98d3-b492fee772a6>
2010-12-12 04:09:13 +0000

    4.0 updates

--- buildroot_4.x/trunk/PacketProtector_mipsel.sh
+++ buildroot_4.x/trunk/PacketProtector_mipsel.sh
@@ -1,12 +1,4 @@
 #/bin/sh
-chmod +x package/base-files/files/etc/easy-rsa/build*
-chmod +x package/base-files/files/etc/easy-rsa/clean-all
-chmod +x package/base-files/files/etc/easy-rsa/revoke-full
-chmod +x package/base-files/files/etc/easy-rsa/sign-req
-chmod +x package/base-files/files/etc/init.d/dns
-chmod +x package/base-files/files/etc/init.d/packetprotector
-chmod +x package/base-files/files/sbin/diskutils
-chmod +x package/base-files/files/sbin/pp_passwd
 mkdir package/base-files/files/etc/mini_httpd
 cd package/webif/files/www
 rm favicon.ico
